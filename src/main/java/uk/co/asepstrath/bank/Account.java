803d452a965b4c61176e46b2722d681400d7a81c
Robert Wilson <robert.wilson1717@gmail.com>
2024-01-24 10:30:20 +0000

    Add Banking Kata Starter Classes for lab

---
+++ src/main/java/uk/co/asepstrath/bank/Account.java
@@ -0,0 +1,14 @@
+package uk.co.asepstrath.bank;
+
+public class Account {
+
+
+    public void deposit(int amount) {
+
+    }
+
+    public int getBalance() {
+        return 0;
+    }
+
+}
---
+++ src/test/java/uk/co/asepstrath/bank/AccountTests.java
@@ -0,0 +1,15 @@
+package uk.co.asepstrath.bank;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AccountTests {
+
+    @Test
+    public void createAccount(){
+        Account a = new Account();
+        Assertions.assertTrue(a != null);
+    }
+
+
+}
