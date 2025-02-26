c0e452bae71d3e49eea077f996e377e65739a023
Holly Higgins SE2023 <holly.higgins.2023@uni.strath.ac.uk>
2025-02-26 05:18:13 +0000

    Updated controller to have the working function

--- src/main/java/uk/co/asepstrath/bank/Controller.java
+++ src/main/java/uk/co/asepstrath/bank/Controller.java
@@ -1,14 +1,12 @@
 package uk.co.asepstrath.bank;
 
-import io.jooby.Context;
 import io.jooby.ModelAndView;
 import io.jooby.StatusCode;
 import io.jooby.annotation.*;
 import io.jooby.exception.StatusCodeException;
 import kong.unirest.core.Unirest;
 import org.slf4j.Logger;
-import io.jooby.*;
-import javax.annotation.PostConstruct;
+import io.jooby.Context;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -25,13 +23,12 @@ public class Controller {
     private final DataSource dataSource;
     private final Logger logger;
 
-
     public Controller(DataSource ds, Logger log) {
         dataSource = ds;
         logger = log;
     }
 
-    @Path("/welcome")
+    @GET("/accounts")
     public ArrayList<String> welcomeFromDB() {
         // Create a connection
         try (Connection connection = dataSource.getConnection()) {
@@ -47,7 +44,7 @@ public class Controller {
                 double balance = set.getDouble("Balance");
                 boolean roundup = set.getBoolean("RoundUp");
 
-                details.add(id + ", " + name + ", " + balance + ", " + roundup + "\n");
+                details.add(id + ", " + name + ", " + balance + ", " + roundup +"\n");
             }
             return details;
         } catch (SQLException e) {
@@ -56,19 +53,32 @@ public class Controller {
             // And return a HTTP 500 error to the requester
             throw new StatusCodeException(StatusCode.SERVER_ERROR, "Database Error Occurred");
         }
-
     }
+
     @GET("/{id}")
-    public String UserDetails(String id, ArrayList<String> details, Context ctx) {
-        int i = 0;
-        String userid = ctx.path("id").toString();
-        for (i = 0; i <= details.size(); i++) {
-            if (userid == id) {
-                return "dog";
+    public String UserDetails(Context ctx) {
+        String userId = ctx.path("id").value(); //sets the id from url as a string
+        try (Connection connection = dataSource.getConnection()) {
+            String query = "SELECT * FROM `Accounts` WHERE `ID` = ?";
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                statement.setString(1, userId); //puts the id into the query
+                ResultSet resultSet = statement.executeQuery();
+    
+                if (resultSet.next()) { //checks if there's an account with the matching id
+                    String id = resultSet.getString("ID");
+                    String name = resultSet.getString("Name");
+                    double balance = resultSet.getDouble("Balance");
+                    boolean roundup = resultSet.getBoolean("RoundUp");
+    
+                    return "ID: " + id + ", Name: " + name + ", Balance: " + balance + ", RoundUp: " + roundup; //returns matching account
+                } else {
+                    return "User not found"; //returns error statement when no matching account is detected
+                }
             }
+        } catch (SQLException e) {
+            logger.error("Database Error Occurred", e);
+              // If something does go wrong this will log the stack trace
+            throw new StatusCodeException(StatusCode.SERVER_ERROR, "Database Error Occurred");
+            // And return a HTTP 500 error to the requester
         }
-        return "bark";
-    }
-}
-
-
+    }}
